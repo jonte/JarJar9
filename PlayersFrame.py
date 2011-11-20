588dd27801f637426a3d0866c59a83b9f6204f9d
Jonatan Pålsson <jonatan.p@gmail.com>
2011-11-20 01:29:34 +0100

    StringVar needs to have master set

--- PlayersFrame.py
+++ PlayersFrame.py
@@ -1,17 +1,20 @@
 from Tkinter import *
 
 class PlayersFrame(Frame):
-    player1Variable = StringVar()
-    player2Variable = StringVar()
+
+    def initVar(self, sv):
+        self.player1Lbl.configure(textvariable=sv)
 
     def __init__(self, parent, **options):
-        Frame.__init__(self, parent.root)
+        Frame.__init__(self, parent)
+        self.player1Variable = StringVar(master = parent)
+        self.player2Variable = StringVar(master = parent)
         self.parent = parent
-        player1Lbl = Label(self, textvariable=self.player1Variable)
-        player2Lbl = Label(self, textvariable=self.player2Variable)
+        self.player1Lbl = Label(self)#variable=self.player1Variable)
+        self.player2Lbl = Label(self, textvariable=self.player2Variable)
 
-        player1Lbl.grid(row=1, column=0, sticky=W)
-        player2Lbl.grid(row=1, column=1, sticky=E)
+        self.player1Lbl.grid(row=1, column=0, sticky=W)
+        self.player2Lbl.grid(row=1, column=1, sticky=E)
 
     def setPlayerText(self, number, name, score):
         if number == 1:
@@ -19,8 +22,8 @@ class PlayersFrame(Frame):
         elif number == 2:
             self.player2Variable.set("%s: %s" % (name, score))
 
-#if __name__ == "__main__":
-#    tk = Tk()
-#    pf = PlayersFrame(tk)
-#    pf.grid(row=0, column=0)
-#    mainloop()
+if __name__ == "__main__":
+    tk = Tk()
+    pf = PlayersFrame(tk)
+    pf.grid(row=0, column=0)
+    mainloop()
